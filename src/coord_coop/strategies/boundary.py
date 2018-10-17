651ce4a5a22aa19cf5b1ae82d61dab2b567049dc
Vince Knight <vincent.knight@gmail.com>
2018-10-17 12:12:35 +0100

    Write utility function and pass through black.

--- src/coord_coop/strategies/boundary.py
+++ src/coord_coop/strategies/boundary.py
@@ -2,12 +2,14 @@ import random
 from coord_coop.actions import C, D
 from .base import BaseStrategy
 
-class SingleBoundaryStrategy():
+
+class SingleBoundaryStrategy:
     """
     A deterministic strategy that defects until a given number of players
     cooperate.
     """
-    def __init__(self, boundary: int, number_of_players: int, p: float):
+
+    def __init__(self, boundary: int, number_of_players: int, p: float) -> None:
         """
         Initialises an instance of the class:
 
@@ -16,5 +18,7 @@ class SingleBoundaryStrategy():
         - number_of_players: the total number of players playing
         - p: probability of cooperating as an initial action.
         """
-        self.sequence = tuple([int(i >= boundary) for i in range(number_of_players)])
+        self.sequence = tuple(
+            [int(i >= boundary) for i in range(number_of_players)]
+        )
         self.action = C if random.random() < p else D
