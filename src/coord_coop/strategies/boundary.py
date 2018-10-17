cc9228652a9f2c9f83d73f85f94dd39759af7b5a
Vince Knight <vincent.knight@gmail.com>
2018-10-17 11:16:07 +0100

    Add type hints.

--- src/coord_coop/strategies/boundary.py
+++ src/coord_coop/strategies/boundary.py
@@ -3,6 +3,18 @@ from coord_coop.actions import C, D
 from .base import BaseStrategy
 
 class SingleBoundaryStrategy():
-    def __init__(self, boundary, number_of_players, p):
+    """
+    A deterministic strategy that defects until a given number of players
+    cooperate.
+    """
+    def __init__(self, boundary: int, number_of_players: int, p: float):
+        """
+        Initialises an instance of the class:
+
+        - boundary: the number of cooperates needed before this player
+          cooperates
+        - number_of_players: the total number of players playing
+        - p: probability of cooperating as an initial action.
+        """
         self.sequence = tuple([int(i >= boundary) for i in range(number_of_players)])
         self.action = C if random.random() < p else D
